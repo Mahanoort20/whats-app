8c7aaa6bb590651667dbb05ea2b0dcc4ec04bde1
Mahanoort20 <49521284+Mahanoort20@users.noreply.github.com>
2019-07-11 17:26:20 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,64 @@
+let whatsApp = {
+    chat=[
+        {
+            name: "Mahanoor",
+            phoneNumber: "03002558913",
+            status: "hey there! i am using what's app",
+            massage: "kesi ho",
+            lastseen: "2:46",
+        },
+        {
+            name: "Uzma",
+            phoneNumber: "03002558913",
+            status: "at the movie",
+            massage: "hi how are you..!",
+            lastseen: "3:00",
+        },
+        {
+            name: "Reena",
+            phoneNumber: "03002558913",
+            status: "Busy",
+            massage: "Are you free today?",
+            lastseen: "9:46",
+        },
+        {
+            name: "Urooj",
+            phoneNumber: "03002558913",
+            status: "bettry about to die",
+            massage: "Hello",
+            lastseen: "10:46",
+        },
+    ]
+status=[
+
+],
+calls=[
+    {
+        name: "Mahanoor",
+        phoneNumbers: "03002558913",
+        Time: "9:00",
+        Date: "10-06-2019"
+    },
+    {
+        name: "Uzma",
+        phoneNumbers: "03002558913",
+        Time: "9:00",
+        Date: "10-06-2019"
+    },
+    {
+        name: "Reena",
+        phoneNumbers: "03002558913",
+        Time: "9:00",
+        Date: "10-06-2019"
+    },
+    {
+        name: "Urooj",
+        phoneNumbers: "03002558913",
+        Time: "9:00",
+        Date: "10-06-2019"
+    },
+    
+    
+]
+
+}
